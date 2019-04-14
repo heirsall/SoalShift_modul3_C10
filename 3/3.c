8d8f6fdc28427d1ca953dd3a918a5a44a3ae114c
zahrul100 <39825311+zahrul100@users.noreply.github.com>
2019-04-14 13:56:48 +0700

    Update 3.c

--- 3/3.c
+++ 3/3.c
@@ -10,9 +10,10 @@ int angka=0;
 int stat = 1;
 int agmal = 0,iraj =100;
 int bakmal = 0,bakraj = 0;
-int tidur=0;
 int length=0;//inisialisasi jumlah looping
 char print[]=""; //="print";
+int bangun=1;
+int tidur=1;
 void* playandcount(void *arg)
 {
     unsigned long i=0;
@@ -21,35 +22,62 @@ void* playandcount(void *arg)
      
     if(pthread_equal(id,tid[0]))//thread untuk menjalankan counter
     {  while(stat){
-	scanf("%[^\n]%*c",print);
-}
+	if(bakmal == 3){
+		tidur = 0;
+		
+		bakmal =0;
+		sleep(10);
+		tidur = 1;
+	
+	}
+	
+	
+	}
 					
     }
 
     else if(pthread_equal(id,tid[1]))
     {	
-	char temp[100];	
 	while(stat){
-	strcpy(temp,"All Status");
-      if(strcmp(print,temp)==0){
-      	print[0]='\0';
-	printf("Agmal = %d \nIraj = %d\n",agmal,iraj);		
-	  printf("%s\n",print);
+      if(bakraj == 3){
+   		
+		bangun = 0;
+		
+      	bakraj = 0;
+      	sleep(10);
+      	bangun = 1;
+	  }
 	}
-	}
-	}
-   else if(pthread_equal(id,tid[2]))
-    {	
-	char temp[100];
-	while(stat){
-	
-	strcpy(temp,"Agmal Ayo Bangun");
-	if(iraj<=0 || agmal >=100){
-	printf("Agmal Bangun,Dan Berangkat Bersama Agmal");	
-	exit(0);	
 	}
 
-      if(strcmp(print,temp)==0){
+}
+
+
+int main(void)
+{
+    int i=0;
+    int err;
+  
+  
+  
+  
+
+  pthread_create(&(tid[0]),NULL,&playandcount,NULL);//membuat thread
+
+pthread_create(&(tid[1]),NULL,&playandcount,NULL);//membuat thread
+  while(1){
+ 	
+	scanf("%[^\n]%*c",print);
+  	
+	  if(strcmp(print,"All Status")==0){
+      	print[0]='\0';
+	printf("Agmal = %d \nIraj = %d\n",agmal,iraj);		
+        printf("%s\n",print);
+	}
+ 	
+
+
+  	 if(strcmp(print,"Agmal Ayo Bangun")==0 && bangun ==1){
 	agmal = agmal + 15;
 	//printf("Agmal sekarang = %d\n",agmal);
 	
@@ -58,63 +86,38 @@ void* playandcount(void *arg)
 
 	
 	}
-	if(bakraj == 3){
-	printf("Fitur Agmal ayo bangun disabled 10 s\n");
-	sleep(10);
-	bakraj = 0;
-	}
-	}
-}
-    else if(pthread_equal(id,tid[3]))
-    {	
-	char temp[100];
-	while(stat){
-	
-	strcpy(temp,"Iraj Ayo Tidur");
-	if(iraj<=0 || agmal >=100){
-	printf("Iraj Ikut Tidur,Dan Bangun Kesiangan Bersama Agmal");	
-	exit(0);	
-	}
 
-      if(strcmp(print,temp)==0){
+	else if(strcmp(print,"Iraj Ayo Tidur")==0 && tidur == 1){
 	iraj = iraj - 20;
-	//printf("Iraj sekarang = %d\n",iraj);
 	
 	print[0]='\0';	
 	bakraj = bakraj + 1;
 
 	
 	}
+
 	if(bakmal == 3){
-	printf("Fitur Iraj ayo tidur disabled 10 s\n");
-	sleep(10);
-	bakmal = 0;
+		printf("Fitur Iraj ayo tidur disabled 10 s\n");}
+		
+      if(bakraj == 3){
+   		
+		printf("Fitur Agmal ayo bangun disabled 10 s\n");}
+			
+	if(iraj<=0 || agmal >=100){
+	printf("Iraj Ikut Tidur,Dan Bangun Kesiangan Bersama Agmal");	
+	exit(0);	
 	}
-	}
-}
-}
+	 if(agmal >= 100){
+	 printf("Agmal Ikut Bangun,Dan Olahraga Bersama Iraj");
+	 }
 
-
-int main(void)
-{
-    int i=0;
-    int err;
-    while(i<4)//looping membuat thread 2x
-    {
-        err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);//membuat thread
-        if(err!=0)//cek error
-        {
-            printf("\n can't create thread : [%s]",strerror(err));
-        }
-        else
-        {
-//            printf("\n create thread success");
-        }
-        i++;
-    }
-    pthread_join(tid[0],NULL);
-    pthread_join(tid[1],NULL);
-    pthread_join(tid[2],NULL);
-    pthread_join(tid[3],NULL);
+  
+  }
+  
+  
+  
+  
+  
+  
     return 0;
 }
